d2ce265d6ab06f523f75d703ab66f2eb4ed2d8f6
KamaliHari <kamalihari2002@gmail.com>
2022-07-30 10:45:10 +0530

    first

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('Shopkeeper/home', { title: 'Express' });
 });
+
+router.get('/', function(req, res, next) {
+  res.render('Shopkeeper/home', { title: 'Home' });
+});
+router.get('/book', function(req, res, next) {
+  res.render('booking', { title: 'Home' });
+});
+
+
 
 module.exports = router;
